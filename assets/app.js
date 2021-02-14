1002e0ad0c46ab2a3ba490abc5b4b7dfbfae24e1
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-02-15 00:25:14 +0100

    😂🔨Done creating joke generator

---
+++ assets/app.js
@@ -0,0 +1,20 @@
+const jokeText = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokebtn");
+
+jokeBtn.addEventListener("click", generateJokes);
+
+generateJokes()
+
+async function generateJokes() {
+    const res = await fetch(
+        "http://v2.jokeapi.dev/joke/Programming,Dark,Spooky?blacklistFlags=nsfw,religious,political,racist,sexist,explicit"
+    )
+    const data = await res.json();
+    let joke = ""
+    if (data.joke == undefined) {
+        joke = `${data.setup} <br /> ${data.delivery}`
+    } else {
+        joke = data.joke
+    }
+    jokeText.innerHTML = joke
+}
